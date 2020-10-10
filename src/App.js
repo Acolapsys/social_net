f52067e6a516b4ec37db727647bb05a39314354e
Acolapsys <timur_sh@bk.ru>
2020-10-11 01:35:35 +0500

    init: start social network project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Sidebar from './components/Sidebar'
+import Profile from './components/Profile'
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="wrapper">
+          <div className="main__wrapper">
+            <Sidebar />
+            <Profile />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContentHeader from './Header'
+
+const Profile = () => {
+  return (
+    <section className="content">
+      <ContentHeader />
+      <div className="content__banner"></div>
+      <div className="content__info">ava + info</div>
+      <div className="content__new">
+        My posts
+        <div className="content__new-post">New post</div>
+      </div>
+      <div className="content__posts">
+        <div className="content__post">Post 1</div>
+        <div className="content__post">Post 2</div>
+      </div>
+    </section>
+  );
+};
+
+export default Profile;
